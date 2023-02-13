5d76228e6a42c59687dd1d52e8fcef0a720a129d
COURAGE2020 <70186043+COURAGE2020@users.noreply.github.com>
2023-02-13 01:34:12 -0500

    Update glueTestScript.py

--- glueTestScript.py
+++ glueTestScript.py
@@ -1,28 +1,46 @@
-import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
-
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
-
-## Add your code here to transform your data
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "mydatabase", table_name = "mytable", transformation_ctx = "datasource0")
-
-applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("column1", "string", "column1", "string"), ("column2", "string", "column2", "double"), ("column3", "string", "column3", "string")], transformation_ctx = "applymapping1")
-
-resolvechoice2 = ResolveChoice.apply(frame = applymapping1, choice = "make_cols", transformation_ctx = "resolvechoice2")
-
-dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
-
-datasink4 = glueContext.write_dynamic_frame.from_options(frame = dropnullfields3, connection_type = "s3", connection_options = {"path": "s3://mybucket/output"}, format = "parquet", transformation_ctx = "datasink4")
-
-job.commit()
+AWSTemplateFormatVersion: '2010-09-09'
+Parameters:
+  GitHubScript:
+    Type: String
+    Description: Name of the script file stored in the Github repository
+  GitHubRepo:
+    Type: String
+    Description: URL of the Github repository
+Resources:
+  GlueJob:
+    Type: 'AWS::Glue::Job'
+    Properties:
+      Command:
+        Name: 'pythonshell'
+        ScriptLocation: !Sub 'https://raw.githubusercontent.com/${GitHubRepo}/master/${GitHubScript}'
+      DefaultArguments:
+        '--job-bookmark-option': 'job-bookmark-disable'
+      Description: 'A Glue Job to pull a script from a Github repository'
+      ExecutionProperty:
+        MaxConcurrentRuns: 1
+      Name: !Ref 'AWS::StackName'
+      Role: !GetAtt [GlueServiceRole, Arn]
+      Timeout: 2880
+      MaxRetries: 0
+  GlueServiceRole:
+    Type: 'AWS::IAM::Role'
+    Properties:
+      AssumeRolePolicyDocument:
+        Version: '2012-10-17'
+        Statement:
+          - Effect: 'Allow'
+            Principal:
+              Service:
+                - 'glue.amazonaws.com'
+            Action: 'sts:AssumeRole'
+      Policies:
+        - PolicyName: 'GlueServiceRolePolicy'
+          PolicyDocument:
+            Version: '2012-10-17'
+            Statement:
+              - Effect: 'Allow'
+                Action:
+                  - 'glue:GetJobRuns'
+                  - 'glue:BatchGetJobs'
+                  - 'glue:StartJobRun'
+                Resource: '*'
